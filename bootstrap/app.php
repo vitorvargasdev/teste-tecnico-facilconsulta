954b6b5371eb132e296c5550071f2e1a4b73b002
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-02-01 17:59:08 -0300

    feat: add api route file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withRouting(
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'api',
+    )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
