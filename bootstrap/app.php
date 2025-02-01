d456da60fc02ab143a28facbf8ffd5b4afba6e61
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-02-01 18:44:55 -0300

    feat: implement basic auth with JWT

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withRouting(
-        api: __DIR__.'/../routes/api.php',
-        apiPrefix: 'api',
+        commands: __DIR__ . '/../routes/console.php',
+        using: function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get("/", function () {
-    return "Hello World!";
-});
+Route::post('login', [AuthController::class, 'login']);
+Route::get('user', [AuthController::class, 'me']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
